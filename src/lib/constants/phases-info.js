a8e44c50377caa7c6b7b01f5dc698e518dd56392
Dorian <117446752+DorianTrazos@users.noreply.github.com>
2025-02-28 21:04:19 +0100

    fix time

--- src/lib/constants/phases-info.js
+++ src/lib/constants/phases-info.js
@@ -1,7 +1,7 @@
 const GAME_PHASES = {
-	MEMORIZE: { title: 'Memoriza', time: 2 },
+	MEMORIZE: { title: 'Memoriza', time: 20 },
 	READY: { title: '¿Listo?', time: 3 },
-	PLAY: { title: '¿Qué Ha Cambiado?', time: 20 }
+	PLAY: { title: '¿Qué Ha Cambiado?', time: 120 }
 };
 
 export const PHASES_INFO = {
